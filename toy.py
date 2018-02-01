14fcfa29dd6a64ad3e4f23372392aee54949e6fb
Alessandro Valli <av@oblong.com>
2018-02-01 19:09:52 +0100

    fps

--- toy.py
+++ toy.py
@@ -8,17 +8,11 @@ import xwiimote
 import glob
 
 ''''
-pygame.transform.scale()
-resize to new resolution
-scale(Surface, (width, height), DestSurface = None) -> Surface
-
-
 Make a copy of the image you want to show (to not change the original) and use following:
 self.image = self.original_image.copy()
 # this works on images with per pixel alpha too
 alpha = 128
 self.image.fill((255, 255, 255, alpha), None, pygame.BLEND_RGBA_MULT)
-
 
 def blit_alpha(target, source, location, opacity):
         x = location[0]
@@ -28,9 +22,7 @@ def blit_alpha(target, source, location, opacity):
         temp.blit(source, (0, 0))
         temp.set_alpha(opacity)
         target.blit(temp, location)
-
-
-If you replace the screen.blit(happy, (100, 100)) with a call to blit_alpha(screen, happy, (100, 100), 128), you get the following:
+Replace the screen.blit(happy, (100, 100)) with a call to blit_alpha(screen, happy, (100, 100), 128)
 '''''
 
 def IsBoard(dev):
@@ -77,14 +69,19 @@ use_board = False
 iface = None
 if use_board:
     iface, board_p = InitBoard()
+
+# pygame init
 pygame.init()
 display = (1920, 1080)
 screen = pygame.display.set_mode(display, DOUBLEBUF)  # |FULLSCREEN)
 clock = pygame.time.Clock()
+
+# fonts
 # print(pygame.font.get_fonts())
 font = pygame.font.SysFont("ubuntu", 72)
 text = font.render("Hello, World", True, (255, 255, 255))
 
+# sounds
 sound_files = glob.glob('sounds/*.wav')
 print(sound_files)
 effects = []
@@ -92,18 +89,19 @@ for file in sound_files:
     effects.append(pygame.mixer.Sound(file))
 sound_count = 0
 
+# images
 image = pygame.image.load('images/glow.png')
 
-i = 0
+frames = 0
+before = time.time()
 
 while True:
     if iface == None:
         m = numpy.array((0, 0, 0, 0))
     else:
         m = GetMeasurementsFromBoard(iface, board_p)
-    if i == 10:
+    if frames == 10:
         zero = m
-    i = i + 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -119,16 +117,19 @@ while True:
             effect.play()
             sound_count = sound_count + 1
     screen.fill((100, 100, 100))
-    if i > 11:
+    if frames > 11:
         press = m - zero
         # print(press)
         if numpy.sum(press) > 10.0:
             x = 4.0 * ((press[1] + press[3]) - (press[0] + press[2])) / (numpy.sum(press))
             y = 2.0 * ((press[0] + press[1]) - (press[2] + press[3])) / (numpy.sum(press))
             screen.blit(image, (int(x * 500 + 900), int(- y * 500 + 500)))
-            print(x, y)
+            # print(x, y)
     screen.blit(text, (320 - text.get_width() // 2, 240 - text.get_height() // 2))
     pygame.display.flip()
-    # pygame.time.wait(10)
+    # pygame.time.wait(15)
+    # time.sleep(1.0 / 90.0)
     clock.tick(60)
-
+    frames = frames + 1
+    if frames % 60 == 0:
+        print('fps %.1f' % (float(frames) / (time.time() - before)))
