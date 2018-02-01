2cac9499ae7f2a0680a22b3a6cf79fd28b88f7fe
Alessandro Valli <av@oblong.com>
2018-02-01 18:31:29 +0100

    works with board and without

--- toy.py
+++ toy.py
@@ -43,6 +43,7 @@ def WaitForBoard():
     mon = xwiimote.monitor(True, False)
     dev = None
     while True:
+        print('waiting for board')
         mon.get_fd(True) # blocks
         connected = mon.poll()
         if connected == None:
@@ -55,24 +56,27 @@ def WaitForBoard():
             print("Waiting...")
     return dev
 
-def GetMeasurementsFromBoard(iface):
-    p = select.epoll.fromfd(iface.get_fd())
-    while True:
-        p.poll(16)  # with no argument, it blocks
-        event = xwiimote.event()
-        iface.dispatch(event)
-        tl = event.get_abs(2)[0]
-        tr = event.get_abs(0)[0]
-        bl = event.get_abs(3)[0]
-        br = event.get_abs(1)[0]
-        yield numpy.array((tl,tr,bl,br)) / 100.0
+def GetMeasurementsFromBoard(iface, board_p):
+    board_p.poll(16)  # with no argument, it blocks
+    event = xwiimote.event()
+    iface.dispatch(event)
+    tl = event.get_abs(2)[0]
+    tr = event.get_abs(0)[0]
+    bl = event.get_abs(3)[0]
+    br = event.get_abs(1)[0]
+    return numpy.array((tl,tr,bl,br)) / 100.0
 
 def InitBoard():
     iface = xwiimote.iface(WaitForBoard())
     iface.open(xwiimote.IFACE_BALANCE_BOARD)
-    return iface
+    board_p = select.epoll.fromfd(iface.get_fd())
+    return iface, board_p
 
-iface = InitBoard()
+use_board = False
+
+iface = None
+if use_board:
+    iface, board_p = InitBoard()
 pygame.init()
 display = (1920, 1080)
 screen = pygame.display.set_mode(display, DOUBLEBUF)  # |FULLSCREEN)
@@ -92,35 +96,37 @@ image = pygame.image.load('images/glow.png')
 
 i = 0
 
-try:
-    for m in GetMeasurementsFromBoard(iface):
-        if i == 10:
-            zero = m
-        i = i + 1
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                print('quit')
-                quit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
-                print(sound_count)
-                effect = effects[sound_count]
-                effect.play()
-                sound_count = sound_count + 1
-        screen.fill((100, 100, 100))
-        if i > 11:
-            press = m - zero
-            # print(press)
-            if numpy.sum(press) > 10.0:
-                x = 4.0 * ((press[1] + press[3]) - (press[0] + press[2])) / (numpy.sum(press))
-                y = 2.0 * ((press[0] + press[1]) - (press[2] + press[3])) / (numpy.sum(press))
-                screen.blit(image, (int(x * 500 + 900), int(- y * 500 + 500)))
-                print(x, y)
-        screen.blit(text, (320 - text.get_width() // 2, 240 - text.get_height() // 2))
-        pygame.display.flip()
-        # pygame.time.wait(10)
-        clock.tick(60)
-except:
-    quit()
+while True:
+    if iface == None:
+        m = numpy.array((0, 0, 0, 0))
+    else:
+        m = GetMeasurementsFromBoard(iface, board_p)
+    if i == 10:
+        zero = m
+    i = i + 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            print('quit')
+            quit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            pygame.quit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+            print(sound_count)
+            effect = effects[sound_count]
+            effect.play()
+            sound_count = sound_count + 1
+    screen.fill((100, 100, 100))
+    if i > 11:
+        press = m - zero
+        # print(press)
+        if numpy.sum(press) > 10.0:
+            x = 4.0 * ((press[1] + press[3]) - (press[0] + press[2])) / (numpy.sum(press))
+            y = 2.0 * ((press[0] + press[1]) - (press[2] + press[3])) / (numpy.sum(press))
+            screen.blit(image, (int(x * 500 + 900), int(- y * 500 + 500)))
+            print(x, y)
+    screen.blit(text, (320 - text.get_width() // 2, 240 - text.get_height() // 2))
+    pygame.display.flip()
+    # pygame.time.wait(10)
+    clock.tick(60)
+
