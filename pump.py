4e87ddda2cb81293a600d8543f0aec8d29702153
Alessandro Valli <av@naturalinteraction.org>
2018-04-16 14:53:14 +0200

    pump and lamps on pins 2 and 3

--- pump.py
+++ pump.py
@@ -15,15 +15,19 @@ pi = pigpio.pi()
 
 while True:
     localtime = time.localtime(time.time())
+    if localtime.tm_hour >= 11 and localtime.tm_hour <= 23:
+        pi.write(2, 0)  # lamps
+    else:
+        pi.write(2, 1)  # lamps
     if localtime.tm_sec < 30 and localtime.tm_min % 15 == 0 and (localtime.tm_hour >= 0 or localtime.tm_hour == 0):  # day and night
         message = str(localtime.tm_hour) + ':' + str(localtime.tm_min) + ':' + str(localtime.tm_sec) + ' pump on'
         print(message)
         pump_log = pump_log + message + '<br>'
         UpdateWeb(pump_log)
-        pi.write(2, 0)
+        pi.write(3, 0)
         time.sleep(40)
         print(str(localtime.tm_hour) + ':' + str(localtime.tm_min) + ':' + str(localtime.tm_sec) + ' pump off')
-        pi.write(2, 1)
+        pi.write(3, 1)
     else:
         print(str(localtime.tm_hour) + ':' + str(localtime.tm_min) + ':' + str(localtime.tm_sec) + ' nop')
     if localtime.tm_min == 0 and localtime.tm_sec < 30:
--- timer.py
+++ timer.py
@@ -12,19 +12,19 @@ pi = pigpio.pi()
 pi.write(2, 0)  # turn on relay at startup
 time.sleep(1)
 pi.write(3, 0)  # turn on relay at startup
-time.sleep(1)
-pi.write(4, 0)  # turn on relay at startup
-time.sleep(1)
-pi.write(14, 0)  # turn on relay at startup
-time.sleep(1)
+#time.sleep(1)
+#pi.write(4, 0)  # turn on relay at startup
+#time.sleep(1)
+#pi.write(14, 0)  # turn on relay at startup
+#time.sleep(1)
 
 time.sleep(5)
 
 pi.write(2, 1)  # turn off relay at any keypress
 time.sleep(1)
 pi.write(3, 1)  # turn off relay at any keypress
-time.sleep(1)
-pi.write(4, 1)  # turn off relay at any keypress
-time.sleep(1)
-pi.write(14, 1)  # turn off relay at any keypress
+#time.sleep(1)
+#pi.write(4, 1)  # turn off relay at any keypress
+#time.sleep(1)
+#pi.write(14, 1)  # turn off relay at any keypress
 
